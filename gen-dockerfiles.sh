e9a813e541006c1e3f351d76ecde0b5f5a08aae9
Peter Kazazes <peter@peterk.co>
2018-11-14 02:08:58 -0800

    mv

--- gen-dockerfiles.sh
+++ gen-dockerfiles.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-sed 's/resin\/%%BALENA_MACHINE_NAME%%-//' Dockerfile.template > gnuradio-full-amd64.dockerfile
-sed 's/%%BALENA_MACHINE_NAME%%/odroid-xu4/' Dockerfile.template > gnuradio-resin-armhf.dockerfile
+sed 's/resin\/%%BALENA_MACHINE_NAME%%-//' Dockerfile.template > gnuradio-etc-amd64.dockerfile
+sed 's/%%BALENA_MACHINE_NAME%%/odroid-xu4/' Dockerfile.template > gnuradio-etc-armhf.dockerfile
